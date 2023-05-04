79b9d88f713c581587e2d5ca7ebe39c92ebd7ec4
Adrian Ensan <adrianensan@gmail.com>
2023-05-04 14:06:42 -0400

    SUpport reversing stack items

--- app/ui/components/Stack.swift
+++ app/ui/components/Stack.swift
@@ -7,15 +7,18 @@ public struct Stack<Content: View>: View {
   var orientation: Orientation
   var alignment: Alignment
   var spacing: CGFloat
+  var isReversed: Bool
   var content: Content
   
   public init(orientation: Orientation,
               alignment: Alignment = .center,
               spacing: CGFloat = 0,
+              isReversed: Bool = false,
               @ViewBuilder content: () -> Content) {
     self.orientation = orientation
     self.alignment = alignment
     self.spacing = spacing
+    self.isReversed = isReversed
     self.content = content()
   }
   
@@ -24,11 +27,11 @@ public struct Stack<Content: View>: View {
     case .horizontal:
       HStack(alignment: alignment.vertical, spacing: spacing) {
         content
-      }
+      }.environment(\.layoutDirection, isReversed ? .rightToLeft : .leftToRight)
     case .vertical:
       VStack(alignment: alignment.horizontal, spacing: spacing) {
         content
-      }
+      }.environment(\.layoutDirection, isReversed ? .rightToLeft : .leftToRight)
     }
   }
 }
