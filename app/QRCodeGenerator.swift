70fde2c99977337a4f4570146e2c4275758d57c3
Adrian Ensan <adrianensan@gmail.com>
2024-03-26 00:35:45 -0400

    QR Code generation fix

--- app/QRCodeGenerator.swift
+++ app/QRCodeGenerator.swift
@@ -3,18 +3,21 @@ import CoreImage
 import HelloApp
 
 public enum QRCodeGenerator {
-  public func generate(from string: String) -> NativeImage? {
+  public static func generate(from string: String) -> NativeImage? {
     guard let data = string.data(using: String.Encoding.ascii) else { return nil }
     
     let filter = CIFilter.qrCodeGenerator()
     filter.message = data
     
-    guard let ciImage = filter.outputImage else { return nil }
+    guard 
+      let ciImage = filter.outputImage,
+      let cgImage = CIContext().createCGImage(ciImage, from: ciImage.extent)
+    else { return nil }
     
-    return NativeImage(ciImage: ciImage)
+    return NativeImage(cgImage: cgImage)
   }
   
-  public func generateWiFi(ssid: String, password: String, type: String = "WPA") -> NativeImage? {
+  public static func generateWiFi(ssid: String, password: String, type: String = "WPA") -> NativeImage? {
     generate(from: "WIFI:S:\(ssid);T:\(type);P:\(password);;")
   }
 }
